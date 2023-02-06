16009b94c025cc816d4e5b16208771f2b08f6958
Mike Gudenau <mikegudenau@gmail.com>
2023-02-06 09:42:38 -0800

    solution to fizzbuzz challenge

---
+++ codeChallenges/fizzbuzz.js
@@ -0,0 +1,12 @@
+let num = 15;
+
+
+if (num % 3 === 0 && num % 5 === 0) {
+   console.log('fizzbuzz');
+} else if (num % 5 === 0) {
+   console.log('buzz');
+} else {
+   console.log('fizz');
+}
+
+
